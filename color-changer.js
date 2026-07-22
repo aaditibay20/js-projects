46818a322fb82447207a853aa7de5b9d8b6502fa
aaditibay20 <aaditi.bay20@gmail.com>
2026-07-22 17:03:59 +0545

    Add files via upload

---
+++ color-changer.js
@@ -0,0 +1,36 @@
+
+// generate a random color
+
+const randomColor = function(){
+    const hex = '0123456789ABCDEF';
+    let color = "#";
+    for(i =0; i<6; i++){
+        color += hex[Math.floor(Math.random() *16)]
+
+    }
+    return color;
+};
+let intervalId;
+const startChangingcolor = function(){
+if (!intervalId) {
+  intervalId = setInterval(changeBgColor, 1000);
+
+    
+}
+function changeBgColor(){
+      document.body.style.backgroundColor = randomColor();
+}
+
+};
+const stopChangingcolor = function(){
+    clearInterval(intervalId);
+    intervalId = null;
+};
+
+document.querySelector('#start').addEventListener('click', startChangingcolor);
+
+
+document.querySelector('#stop').addEventListener('click', stopChangingcolor);
+
+
+
